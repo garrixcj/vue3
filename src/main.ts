2ce41da1c82166561e1195ccfe3e618db5f89716
jasper_chen <jasper@mail.com>
2021-12-21 10:22:29 +0800

    update:[admin]Jasper-新增設定檔

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import '@/assets/scss/style.scss';
+import '@mdi/font/scss/materialdesignicons.scss';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import '@/components/style/scss/_element-default.scss';
+import '@/components/style/scss/_element-cust.scss';
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(ElementPlus).use(store).use(router).mount('#app');
