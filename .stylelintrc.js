fdcd238537349fe74af4358430c0163649ad6563
hercules_lin <hercules_lin@mail.chungyo.net>
2022-04-27 15:02:55 +0800

    update:[stylelint]Hercules-整理規則順序

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,16 +8,16 @@ module.exports = {
   plugins: ['stylelint-prettier'],
   rules: {
     indentation: 2,
+    'at-rule-no-unknown': null,
     'block-opening-brace-space-before': 'always',
     'declaration-colon-space-after': 'always',
     'font-family-name-quotes': 'always-where-recommended',
     'font-family-no-missing-generic-family-keyword': null,
-    'at-rule-no-unknown': null,
     'no-descending-specificity': null,
     'no-invalid-position-at-import-rule': null,
-    'scss/no-global-function-names': null,
-    'scss/at-if-no-null': null,
     'scss/at-extend-no-missing-placeholder': null,
+    'scss/at-if-no-null': null,
+    'scss/no-global-function-names': null,
     'prettier/prettier': [
       true,
       {
