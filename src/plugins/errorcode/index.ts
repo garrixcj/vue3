d8c287a261bd79b52e7499c9e80b67b8a5151b48
PID-Fanjii_Tsai <fanjii_tsai@mail.chungyo.net>
2022-06-21 10:51:07 +0800

    update:[errorcode]fanjii-更新

--- src/plugins/errorcode/index.ts
+++ src/plugins/errorcode/index.ts
@@ -1,8 +1,13 @@
 /**
  * 統整底層用 Errorcode 對應表
- * Todo lang.json要換掉
  */
-import dict from './lang.json';
+import dict from '@/language/error/msg.json';
+import type { ResponseData } from '@/http/typings';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import { useTrans } from '@/plugins/i18n/replace';
+
+dayjs.extend(utc);
 
 export const codeMap: Record<string, number[] | number> = {
   // 找不到路由對應
@@ -43,50 +48,78 @@ export type HexResponse = {
   body?: string;
 };
 
-// 轉譯
-export const codeT = (
-  lang: CodeLang,
-  key: CodeKey,
-  option?: { [key: string]: string },
-): string => {
-  if (option) {
-    return Object.keys(option).reduce(
-      (acc, opt) => acc.replace(`{${opt}}`, option[opt]),
-      dict[lang][key],
-    );
-  }
-  return dict[lang][key];
+// 取得 errorcode 轉譯功能
+export const useCodeTrans = (lang: CodeLang) => {
+  const { t } = useTrans(dict, lang);
+  // error code文本格式
+  const getMsg = (response: ResponseData, type = 'error') => {
+    let msg = '';
+
+    // 通用錯誤訊息
+    if (response.code && t(`${response.code}` as CodeKey)) {
+      msg = `${t(`${response.code}` as CodeKey)}`;
+    } else if (type === 'warning') {
+      // warning預設'無效的操作'
+      msg = `${t('invalid_operation')}`;
+    } else {
+      // error預設'系統錯誤'
+      msg = `${t('system_error')}`;
+    }
+    // error類型要加上error code及response_code
+    if (type === 'error') {
+      msg += `(${response.code})`;
+      // 加上response_code
+      if (response.response_code) {
+        msg += `#${response.response_code}`;
+      }
+    }
+
+    msg = msg.toString();
+
+    return msg;
+  };
+
+  // 線路異常時，無法取得錯誤的物件時，包裝錯誤頁面的 body 內容
+  const getBodyMsg = (body: string) => {
+    let msg = '';
+    const time = dayjs().utcOffset(-4).format('YYYY-MM-DD HH:mm:ss');
+    // 線路問題導致4xx或5xx等錯誤的情況
+    msg = `${codeT('system_error')}( ${time} ) ${body}`;
+    msg = msg.toString();
+
+    return msg;
+  };
+
+  return { t, getMsg, getBodyMsg };
 };
+
+export const { t: codeT } = useTrans(dict);
 
 // error code文本格式
 export const getMsg = (
   lang: CodeLang,
-  response: HexResponse,
+  response: ResponseData,
   type = 'error',
 ) => {
   let msg = '';
 
   // 通用錯誤訊息
-  if (response.code && codeT(lang, response.code)) {
-    msg = `${codeT(lang, response.code)}`;
+  if (response.code && codeT(`${response.code}` as CodeKey, {}, lang)) {
+    msg = `${codeT(`${response.code}` as CodeKey, {}, lang)}`;
   } else if (type === 'warning') {
     // warning預設'無效的操作'
-    msg = `${codeT(lang, 'invalid_operation')}`;
+    msg = `${codeT('invalid_operation', {}, lang)}`;
   } else {
     // error預設'系統錯誤'
-    msg = `${codeT(lang, 'system_error')}`;
+    msg = `${codeT('system_error', {}, lang)}`;
   }
   // error類型要加上error code及response_code
   if (type === 'error') {
-    msg += `(${response.code as string})`;
+    msg += `(${response.code})`;
     // 加上response_code
     if (response.response_code) {
       msg += `#${response.response_code}`;
     }
-  }
-  // 線路問題導致4xx或5xx等錯誤的情況
-  if (type === 'network') {
-    msg = `${codeT(lang, 'system_error')}( ${response.time} ) ${response.body}`;
   }
 
   msg = msg.toString();
@@ -94,11 +127,18 @@ export const getMsg = (
   return msg;
 };
 
-// 末三碼大於500為warning
-export const getType = (code: number | string) =>
-  +code % 1000 > 500 ? 'warning' : 'error';
+// 線路異常時，無法取得錯誤的物件時，包裝錯誤頁面的 body 內容
+export const getBodyMsg = (lang: CodeLang, body: string) => {
+  let msg = '';
+  const time = dayjs().utcOffset(-4).format('YYYY-MM-DD HH:mm:ss');
+  // 線路問題導致4xx或5xx等錯誤的情況
+  msg = `${codeT('system_error', {}, lang)}( ${time} ) ${body}`;
+  msg = msg.toString();
 
-// 判斷是否維護中
+  return msg;
+};
+
+// 判斷 errorcode 是否維護中
 export const isMaintain = (code: number | string) =>
   maintainCodes.includes(+code);
 
