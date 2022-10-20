daa5863ece615feeb0c533b059826e105f639ef2
garrix_chang <garrix_chang@mail.chungyo.net>
2022-10-20 09:29:55 +0800

    fix:[網址管理]Garrix-修正 Code Review 問題

--- src/views/system-setting/url-management/list.ts
+++ src/views/system-setting/url-management/list.ts
@@ -1,6 +1,5 @@
-import { reactive, ref } from 'vue';
+import { ref } from 'vue';
 import { url as urlAPI } from '@/api/domain';
-import { AxiosResponse } from 'axios';
 
 // 站別資訊
 export type SiteInfo = {
@@ -20,12 +19,12 @@ export type SiteOption = {
 };
 
 // 取得「站別」的相關資料
-export const useGetSiteListApi = () => {
+export const useSiteList = () => {
   const siteOptions = ref<SiteOption[]>([]);
 
   // 取得站別列表資料
   const getSiteList = () => {
-    return urlAPI.getSiteList().then((response: AxiosResponse) => {
+    return urlAPI.getSiteList().then(response => {
       if (response.data.result) {
         siteOptions.value = response.data.data.map((item: SiteInfo) => {
           const result = {
@@ -40,52 +39,8 @@ export const useGetSiteListApi = () => {
     });
   };
 
-  // 站別快搜規則
-  const siteQuickSearch = (pattern: string) => {
-    const escapeRegexpString = (value = '') =>
-      String(value).replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
-    const re = new RegExp(escapeRegexpString(pattern), 'i');
-    return siteOptions.value.filter(
-      post => re.test(post.label) || re.test(post.code),
-    );
-  };
-
   return {
     siteOptions,
     getSiteList,
-    siteQuickSearch,
-  };
-};
-
-// 取得「單一站別」詳細資料
-export const useGetSiteDetailApi = () => {
-  const siteDetail = reactive({
-    siteName: '', // 站別
-    prefix: '', // 前置詞
-    suffix: '', // 後置碼
-    env: '', // 環境變數
-    primaryDomain: '', // 主域名
-    remark: '', // 備註
-  });
-
-  // 取得站別詳細資料
-  const getSingleSiteInfo = (site_group: string) => {
-    return urlAPI
-      .getSingleSiteInfo(site_group)
-      .then((response: AxiosResponse) => {
-        if (response.data.result) {
-          siteDetail.siteName = response.data.data.name;
-          siteDetail.prefix = response.data.data.prefix;
-          siteDetail.suffix = response.data.data.login_code;
-          siteDetail.env = response.data.data.env;
-          siteDetail.primaryDomain = response.data.data.main_domain;
-          siteDetail.remark = response.data.data.note;
-        }
-      });
-  };
-
-  return {
-    siteDetail,
-    getSingleSiteInfo,
   };
 };
--- src/views/system-setting/url-management/site-information.vue
+++ src/views/system-setting/url-management/site-information.vue
@@ -1,73 +1,67 @@
 <i18n src="@/languages/system_setting/url_management/index.json"></i18n>
 <template lang="pug">
-rd-drawer.site-information(
-  v-model:visible="visible"
-  :title="t('site_info')"
-  :class="{ 'custom-empty-drawer': isNoData }"
-  :no-fade="isNoData"
-  :loading="loading"
-  @closed="resetSearch"
-)
-  template(#header)
-    rd-form.site-search(
-      ref="drawerRef"
-      inline
-      :model="drawerForm"
-      :rules="drawerRules"
-    )
-      //- 站別
-      rd-form-item(
-        :label="t('site')"
-        prop="site"
+.site-information
+  rd-button(type="default" size="small" @click="visible = !visible") {{ t('site_info') }}
+  rd-drawer(
+    v-model:visible="visible"
+    :title="t('site_info')"
+    :loading="loading"
+    @closed="resetSearch"
+  )
+    template(#header)
+      rd-form.site-search(
+        ref="drawerRef"
         inline
-        size="small"
-        required="required"
+        :model="drawerForm"
+        :rules="drawerRules"
       )
-        rd-select(
-          v-model:value="drawerForm.site"
-          quickSearch
-          :selected-setting="customSelectedSetting"
-          :popper-setting="popperSetting"
-        )
-          rd-option(
-            v-for="(option, index) in siteOptions"
-            :key="index"
-            :value="option.value"
-            :label="option.label"
-            :option="option"
+        //- 站別
+        rd-form-item(:label="t('site')" prop="site" inline size="small")
+          rd-select(
+            v-model:value="drawerForm.site"
+            :quick-search="customSearch"
+            :selected-setting="{ maxWidth: 240 }"
+            :popper-setting="{ width: 'auto' }"
           )
-            template(#suffix)
-              | {{ `[ ${option.code} ]` }}
-          template(#selected="{ current }")
-            span {{ current.label }}
-            span [{{ current.option.code }}]
-      //- 查詢
-      rd-form-item
-        rd-button(size="small" @click="search") {{ t('search') }}
+            rd-option(
+              v-for="(option, index) in siteOptions"
+              :key="index"
+              :value="option.value"
+              :label="option.label"
+              :option="option"
+            )
+              template(#suffix)
+                | {{ `[ ${option.code} ]` }}
+            template(#selected="{ current }")
+              | {{ `${current.label} [${current.option.code}]` }}
+        //- 查詢
+        rd-form-item
+          rd-button(size="small" @click="search") {{ t('search') }}
 
-  //- 主內容
-  before-search-empty(v-if="isBeforeSearch" :label="t('start_search')")
-  .drawer-body(v-if="!isBeforeSearch")
-    //- 基本資料
-    .drawer-body__category(v-if="!isNoData")
-      span {{ t('basic_information') }}
-    rd-grid-table(
-      no-header
-      :class="{ 'no-data-table': isNoData }"
-      :columns="basicInformationColumns"
-      :row="{ background: !isNoData ? 'grey' : '' }"
-      :data-source="basicInformationData"
-    )
+    //- 主內容
+    before-search-empty(v-if="beforeSearch" :label="t('start_search')")
+    .drawer-body(v-else)
+      //- 基本資料
+      .drawer-body__category
+        span {{ t('basic_information') }}
+      rd-divider
+      rd-grid-table(
+        no-header
+        :columns="basicInformationColumns"
+        :row="{ size: 'small' }"
+        :data-source="basicInformationData"
+      )
 </template>
 
 <script lang="ts">
 import { useI18n } from 'vue-i18n';
-import { isEmpty, values } from 'lodash';
+import { isEmpty, keys } from 'lodash';
 import { defineComponent, inject, ref, reactive } from 'vue';
 import RdDrawer from '@/components/custom/drawer/index.vue';
 import BeforeSearchEmpty from '@/components/custom/before-search/empty.vue';
 import RdGridTable from '@/components/custom/grid-table/index.vue';
-import { useGetSiteListApi, useGetSiteDetailApi } from './list';
+import { useSiteList } from './list';
+import { type SiteDetail, useSiteDetail } from './detail';
 
 export default defineComponent({
   name: 'SiteInformation', // 網址管理 - 站別資訊
@@ -80,25 +74,18 @@ export default defineComponent({
     const { t } = useI18n({ useScope: 'local' });
     const loading = ref(true);
     // 站別資訊開關
-    const visible = inject('Visible:siteInfo', false);
+    const visible = ref(false);
     // 搜尋前
-    const isBeforeSearch = ref(true);
+    const beforeSearch = ref(true);
+    // 自定義快搜
+    const customSearch = inject('UrlManagement:customSearch') as Function;
 
     // 站別相關
-    const { getSiteList, siteOptions, siteQuickSearch } = useGetSiteListApi();
+    const { getSiteList, siteOptions } = useSiteList();
     // 取的站別列表
-    getSiteList().finally(() => {
+    getSiteList().then(() => {
       loading.value = false;
     });
-    // 下拉寬度
-    const customSelectedSetting = {
-      minWidth: 240,
-      maxWidth: 240,
-    };
-    // 下拉 popper 寬度
-    const popperSetting = {
-      width: 240,
-    };
 
     // 表單 Ref
     const drawerRef = ref();
@@ -116,80 +103,53 @@ export default defineComponent({
       { dataIndex: 'title', width: 120 },
       { dataIndex: 'info' },
     ];
-    const basicInformationData = ref<{ title: string; info: string }[]>([]);
-    // 搜尋無資料
-    const isNoData = ref(false);
+    const basicInformationData = ref<
+      { title: string | unknown; info: string | unknown }[]
+    >([]);
     // 站別詳細相關
-    const { siteDetail, getSingleSiteInfo } = useGetSiteDetailApi();
+    const { siteDetail, getSingleSiteInfo } = useSiteDetail();
+    const basicInformationField: Record<SiteDetail, string> = {
+      siteName: t('site'),
+      prefix: t('prefix'),
+      suffix: t('suffix'),
+      env: t('env'),
+      primaryDomain: t('primary_domain'),
+      remark: t('remark'),
+    };
     // 點擊搜尋按鈕
     const search = () => {
       drawerRef.value.validate((validate: boolean) => {
         if (validate) {
           loading.value = true;
-          isBeforeSearch.value = false;
-          getSingleSiteInfo(drawerForm.site)
-            .then(() => {
-              isNoData.value = values(siteDetail).every(item => isEmpty(item));
-              if (!isNoData.value) {
-                basicInformationData.value = [
-                  {
-                    title: t('site'),
-                    info: !isEmpty(siteDetail.siteName)
-                      ? siteDetail.siteName
-                      : '--',
-                  },
-                  {
-                    title: t('prefix'),
-                    info: !isEmpty(siteDetail.prefix)
-                      ? siteDetail.prefix
-                      : '--',
-                  },
-                  {
-                    title: t('suffix'),
-                    info: !isEmpty(siteDetail.suffix)
-                      ? siteDetail.suffix
-                      : '--',
-                  },
-                  {
-                    title: t('env'),
-                    info: !isEmpty(siteDetail.env) ? siteDetail.env : '--',
-                  },
-                  {
-                    title: t('primary_domain'),
-                    info: !isEmpty(siteDetail.primaryDomain)
-                      ? siteDetail.primaryDomain
-                      : '--',
-                  },
-                  {
-                    title: t('remark'),
-                    info: !isEmpty(siteDetail.remark)
-                      ? siteDetail.remark
-                      : '--',
-                  },
-                ];
-              }
-            })
-            .finally(() => {
-              loading.value = false;
+          beforeSearch.value = false;
+          getSingleSiteInfo(drawerForm.site).then(() => {
+            basicInformationData.value = (
+              keys(basicInformationField) as SiteDetail[]
+            ).map((key: SiteDetail) => {
+              const result = {
+                title: basicInformationField[key],
+                info: !isEmpty(siteDetail[key]) ? siteDetail[key] : '--',
+              };
+              return result;
             });
+            loading.value = false;
+          });
         }
       });
     };
     // 還原搜尋
     const resetSearch = () => {
       drawerRef.value.resetFields();
-      isBeforeSearch.value = true;
+      beforeSearch.value = true;
     };
 
     return {
       t,
       visible,
       loading,
-      isBeforeSearch,
-      customSelectedSetting,
-      popperSetting,
+      beforeSearch,
       siteOptions,
-      siteQuickSearch,
+      customSearch,
       drawerRef,
       drawerForm,
       drawerRules,
@@ -197,7 +157,6 @@ export default defineComponent({
       resetSearch,
       basicInformationColumns,
       basicInformationData,
-      isNoData,
     };
   },
 });
@@ -206,43 +165,16 @@ export default defineComponent({
 <style lang="scss" scoped>
 .site-information {
   .site-search {
-    margin-top: 15px;
-  }
-
-  .rd-select {
-    :deep(.single__item) {
-      @include flex-basic(space-between);
-      width: 100%;
-    }
+    @include space-vertical(15px);
   }
 
   .drawer-body {
+    @include divider-margin(10px, 10px);
+
     &__category {
-      padding-bottom: 10px;
-      margin-bottom: 10px;
       font-size: 16px;
       color: $text-3;
-      border-bottom: 1px $background-3 solid;
     }
-  }
-}
-// 無資料時擴大畫面
-.custom-empty-drawer {
-  :deep(.el-scrollbar__view) {
-    height: inherit;
-    .drawer-body {
-      height: 100%;
-    }
-  }
-}
-.no-data-table {
-  display: flex;
-  flex-direction: column;
-  height: inherit;
-
-  :deep(.rd-grid-table-empty) {
-    flex-grow: 1;
-    @include flex-basic(center);
   }
 }
 </style>
