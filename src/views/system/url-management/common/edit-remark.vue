459c0aebe14e50534f6c08645e783034b8aec8ec
garrix_chang <garrix_chang@mail.chungyo.net>
2022-11-16 13:59:42 +0800

    update:[網址管理]Garrix-整合編輯備註單一操作、批次操作和 CodeReview 修正

--- src/views/system/url-management/common/edit-remark.vue
+++ src/views/system/url-management/common/edit-remark.vue
@@ -1,113 +1,111 @@
 <i18n src="@/languages/system_setting/url_management/common.json"></i18n>
 <template lang="pug">
 rd-dialog(
-  v-model="dialogVisible"
-  :title="t('edit_remark')"
+  v-model="editRemarkForm.visible"
+  :title="isSingleOperation ? t('edit_remark') : t('batch_remark')"
   :close-on-click-modal="false"
-  :before-close="emitCancelEdit"
   width="520px"
 )
   rd-form(label-width="65px" size="small")
-    rd-form-item(:label="t('site')")
-      span {{ formData.site }}
-    rd-form-item(:label="t('suffix')")
-      span @{{ formData.suffix }}
-    rd-form-item(:label="t('domain_name')")
-      .url-color {{ formData.domainName }}
+    //- 單一操作
+    .single-option(v-if="isSingleOperation")
+      rd-form-item(:label="t('site')")
+        span {{ domainNameData[0].site }}
+      rd-form-item(:label="t('suffix')")
+        span @{{ domainNameData[0].suffix }}
+      rd-form-item(:label="t('domain_name')")
+        rd-link(
+          :href="`http://${domainNameData[0].domainName}`"
+          target="_blank"
+        ) {{ domainNameData[0].domainName }}
+    //- 批次操作
+    .batch-option(v-else)
+      .remark-illustrate {{ t('remark_illustrate_2', { num: domainNameData.length }) }}
+      rd-form-item(:label="t('cover')")
+        rd-radio-group(v-model="editRemarkForm.type")
+          rd-radio(label="cover") {{ t('direct_coverage') }}
+          rd-radio(label="noRemark") {{ t('only_modify_no_remark') }}
+    //- 備註
     rd-form-item(:label="t('remark')")
       rd-input(
-        v-model="formData.remark"
+        v-model="editRemarkForm.remark"
         type="textarea"
         :placeholder="t('please_enter_remarks')"
         show-word-limit
         :maxlength="200"
       )
   template(#footer)
-    rd-button(type="secondary" @click="emitCancelEdit") {{ t('cancel') }}
-    rd-button(type="primary" @click="saveRemark") {{ t('save') }}
+    rd-button(type="secondary" @click="editRemarkForm.visible = false") {{ t('cancel') }}
+    rd-button(type="primary" @click="save") {{ t('save') }}
 </template>
 
 <script lang="ts">
 import { useI18n } from 'vue-i18n';
-import { type PropType, defineComponent, computed } from 'vue';
+import { isEmpty } from 'lodash';
+import { type Ref, defineComponent, inject, computed } from 'vue';
 import { notify } from '@/components/utils/notification';
-import { url as urlAPI } from '@/api/domain';
 
-type FormDataType = 'site' | 'siteName' | 'suffix' | 'domainName' | 'remark';
+type domainNameData = 'site' | 'siteName' | 'suffix' | 'domainName' | 'remark';
+
+type editRemarkForm = {
+  visible: boolean;
+  action: string;
+  type: string;
+  remark: string;
+};
 
 export default defineComponent({
   name: 'EditRemark', // 網址管理 - 編輯備註
-  props: {
-    // 開關
-    visible: {
-      type: Boolean,
-      default: false,
-    },
-    // 編輯資料
-    formData: {
-      type: Object as PropType<Record<FormDataType, string>>,
-      default: () => ({}),
-    },
-  },
-  emits: ['update:visible', 'saved'],
+  emits: ['save'],
   setup(props, { emit }) {
     const { t } = useI18n({ useScope: 'local' });
-
-    // 更新上層的開關
-    const changeVisible = (value: boolean | unknown) => {
-      emit('update:visible', value);
-    };
-
-    // Dialog 開關
-    const dialogVisible = computed({
-      get() {
-        return props.visible;
-      },
-      set(val) {
-        changeVisible(val);
-      },
-    });
-
-    // 取消備註
-    const emitCancelEdit = () => {
-      changeVisible(false);
-    };
-
-    // 儲存備註
-    const saveRemark = () => {
-      if (typeof props.formData !== 'undefined') {
-        return urlAPI
-          .updateDomainNameRemark(
-            [
-              {
-                site_group: props.formData.site,
-                domain_name: props.formData.domainName,
-              },
-            ],
-            1,
-            props.formData.remark,
+    // 域名資料
+    const domainNameData = inject('CustomerDomain:domainNameData') as Ref;
+    // Form 資料
+    const editRemarkForm = inject(
+      'CustomerDomain:editRemarkForm',
+    ) as editRemarkForm;
+    // 單一操作
+    const isSingleOperation = computed(
+      () => editRemarkForm.action === 'single',
+    );
+    // 點擊儲存
+    const save = () => {
+      // 批次操作選擇只修改無備註，且域名資料全部都有備註時，需跳 Warning
+      if (!isSingleOperation.value && editRemarkForm.type === 'noRemark') {
+        if (
+          !domainNameData.value.some((item: Record<domainNameData, string>) =>
+            isEmpty(item.remark),
           )
-          .then(resp => {
-            if (resp.data.result) {
-              notify.success({ title: t('success') });
-            }
-            changeVisible(false);
-          });
+        ) {
+          notify.warning({ title: t('warning'), message: t('warning_msg') });
+          return emit('save', false);
+        }
       }
+      // 單一操作
+      if (isSingleOperation.value) {
+        editRemarkForm.type = 'cover';
+      }
+      return emit('save', true);
     };
-
     return {
       t,
-      dialogVisible,
-      emitCancelEdit,
-      saveRemark,
+      domainNameData,
+      editRemarkForm,
+      isSingleOperation,
+      save,
     };
   },
 });
 </script>
 
 <style lang="scss" scoped>
-.url-color {
-  color: $primary-2;
+.el-form {
+  @include space-vertical(15px);
+}
+.remark-illustrate {
+  margin-bottom: 15px;
+  margin-left: 10px;
+  color: $text-1;
 }
 </style>
