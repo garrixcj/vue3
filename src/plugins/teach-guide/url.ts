d66d2d18f6651e62e8313ca315901596108ba869
garrix_chang <garrix_chang@mail.chungyo.net>
2022-12-05 09:59:26 +0800

    feature:[Plugins元件]Garrix-新增舊版教學連結元件

---
+++ src/plugins/teach-guide/url.ts
@@ -0,0 +1,22 @@
+/**
+ * 系統Host
+ */
+export const host = {
+  bbin: 'http://bbin-tutorial.s3.amazonaws.com/',
+};
+
+/**
+ * Url路徑
+ */
+export const urlRoute = {
+  lock_monitor: 'account_managment/lock_account_monitoring/index.html',
+  interest_wallet: 'promotion-commision/interest_wallet/index.html',
+  user_manager: 'account_managment/manager_account/index.html',
+  commission_search:
+    'report-search/commission_search/member_commission_search/index.html',
+  negative_cash: 'cash_system/negative_balance/index.html',
+  ip_white_list: 'web_management/ip_whitelist_management2/index.html',
+  app_signature: 'web_management/app_signature_management/index.html',
+  domain_management: 'web_management/domain_management/index.html',
+  lost_balance: 'cash_system/missing_amount_of_transfer/index.html',
+};
